84b7eaf40726f60aa29b6c4cdfbdaaa9e4187885
DanMaster23 <mistery.vite.2020@gmail.com>
2026-04-18 16:33:08 +0300

    Изменил код в папке frontend

--- fronttest/src/app/main.tsx
+++ fronttest/src/app/main.tsx
@@ -1,11 +1,7 @@
 
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/index.css'
-import App from './App.tsx'
+  import { createRoot } from "react-dom/client";
+  import App from "./App.tsx";
+  import "./styles/index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  createRoot(document.getElementById("root")!).render(<App />);
+  
